6aee53ffd3a5a4ee80e0bb3672a29fca4c363c9e
Wesley Barbosa da Silva <124535880+wBarbosad@users.noreply.github.com>
2023-04-12 09:19:21 -0300

    Exercicio 02 concluido

--- Listas/Lista3/Exercicio2_3/src/Album.java
+++ Listas/Lista3/Exercicio2_3/src/Album.java
@@ -1,31 +1,31 @@
 import java.util.ArrayList;
 
 public class Album {
-    String nome
-    String nomeArtista
-    ArrayList<Musica> musicas = new ArrayList<>()
+    String nome;
+    String nomeArtista;
+    ArrayList<Musica> musicas = new ArrayList<>();
 
     public Musica getMusicaMaiorPopularidade(){
         int maiorPopularidade = 0;
         int posicao = 0;
         for (int i = 0; i < musicas.size(); i++) {
-            if(musicas[i]>maiorPopularidade){
-                maiorPopularidade=musicas[i];
+            if(musicas.get(i).popularidade>maiorPopularidade){
+                maiorPopularidade = musicas.get(i).popularidade;
                 posicao = i;
             }
         }
-        return musica[posicao];
+        return musicas.get(posicao);
     }
     public  Musica getMusicaMenorDuracao(){
         int menorDuracao = 0;
         int posicao = 0;
         for (int i = 0; i < musicas.size(); i++) {
-            if(musicas[i]>menorDuracao){
-                menorDuracao=musicas[i];
+            if(musicas.get(i).duracaoEmSegundos>menorDuracao){
+                menorDuracao = musicas.get(i).duracaoEmSegundos;
                 posicao = i;
             }
         }
-        return musica[posicao];
+        return musicas.get(posicao);
 
     }
 
--- Listas/Lista3/Exercicio2_3/src/Main.java
+++ Listas/Lista3/Exercicio2_3/src/Main.java
@@ -1,16 +1,26 @@
 public class Main {
     public static void main(String[] args) {
+
         Album album = new Album();
         Musica musica1 = new Musica();
-        Musica musica2 = new Musica();
-
         musica1.duracaoEmSegundos = 70;
+        musica1.popularidade = 500000;
+
+        Musica musica2 = new Musica();
         musica2.duracaoEmSegundos = 80;
+        musica2.popularidade = 6000000;
+
         album.musicas.add(musica1);
         // album > musicas (lista) e adicionar cada uma das músicas que coloquei aqui na lista;
         album.musicas.add(musica2);
 
-        int menorDuracao = album.getMusicaMenorDuracao();
+        Musica menorDuracao = album.getMusicaMenorDuracao();
+        Musica popularidade = album.getMusicaMaiorPopularidade();
+
+        System.out.println(menorDuracao.duracaoEmSegundos);
+        System.out.println(popularidade.popularidade);
+
+
 
     }
 }
