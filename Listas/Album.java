1b45d5be55083db47bbb12964536cecc30045c9a
Wesley Barbosa da Silva <124535880+wBarbosad@users.noreply.github.com>
2023-04-18 10:20:51 -0300

    Lista4

---
+++ Listas/Album.java
@@ -0,0 +1,9 @@
+public class Album{
+    private String nomeAlbum;
+    private String nomeArtista;
+    ArrayList<Musica> musica = new ArrayList(); 
+
+    public Album(String nomeAlbum){ // Construtor album;
+        this.nomeAlbum = nomeAlbum;
+    }
+}
