c16688c44b4af8d38acfde97fa618e8b85a29874
Wesley Barbosa da Silva <124535880+wBarbosad@users.noreply.github.com>
2023-04-27 23:33:27 -0300

    Lista 5

--- Listas/Lista5/Exercicios/Exercicio1/src/Grupo.java
+++ Listas/Lista5/Exercicios/Exercicio1/src/Grupo.java
@@ -3,16 +3,36 @@ import java.util.ArrayList;
 public class Grupo {
     private ArrayList<Heroi> herois;
     private int totalMoedasDeOuro;
-
     public Grupo(){
         this.herois = new ArrayList<>();
     }
 
     public void adicionarHeroi(Heroi heroi){
+        for (int i = 0; i < herois.size(); i++) {
+            if(getHerois().get(i).getId()==heroi.getId()){
+                System.out.println("O heroi de Id = " + herois.get(i).getId() + " já está incluso no grupo!");
+                return;
+            }
+        }
         this.herois.add(heroi);
+        System.out.println("O heroi " + heroi.getNome() + " foi adicionado ao grupo;");
+
+        int somaMoedas = 0;
+        for (int i = 0; i < herois.size(); i++) { //Fazer isso com as moedas.
+            somaMoedas=somaMoedas+herois.get(i).getQuantidadeDeMoedasDeOuro();
+            setTotalMoedasDeOuro(somaMoedas);
+        }
     }
     public void removerHeroi(int idHeroi){
-       // this.herois.remove()
+       int subtrairMoedas = getTotalMoedasDeOuro();
+        for (int i = 0; i < herois.size(); i++) {
+            if (herois.get(i)==herois.get(idHeroi)) {
+                System.out.println("\nO heroi " + herois.get(i).getNome()+ " foi removido do grupo");
+                subtrairMoedas = subtrairMoedas-herois.get(i).getQuantidadeDeMoedasDeOuro();
+                this.herois.remove(idHeroi);
+            }
+        }
+        setTotalMoedasDeOuro(subtrairMoedas);
     }
 
     public int getTotalMoedasDeOuro() {
--- Listas/Lista5/Exercicios/Exercicio1/src/Main.java
+++ Listas/Lista5/Exercicios/Exercicio1/src/Main.java
@@ -1,14 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        int somaMoedas = 0;
         Heroi heroi1 = new Heroi(1, "TremBala", 10);
         Heroi heroi2 = new Heroi(2, "BalaTensa", 20);
+        Heroi heroi3 = new Heroi(3, "SrSalazar", 50);
+        Heroi heroi4 = new Heroi(3, "Masa", 100);
         Grupo grupo = new Grupo();
 
+        System.out.println("\n");
         grupo.adicionarHeroi(heroi1);
         grupo.adicionarHeroi(heroi2);
+        grupo.adicionarHeroi(heroi3);
+        grupo.adicionarHeroi(heroi4);
 
-        for (int i = 0; i < grupo.getHerois().size(); i++) { //Fazer isso com as moedas.
-            System.out.println(grupo.getHerois().get(i).getNome());
-        }
+        grupo.removerHeroi(1);
+
+        System.out.println("\nO grupo tem = " + grupo.getTotalMoedasDeOuro() + " moedas!");
+
+
+
     }
 }
