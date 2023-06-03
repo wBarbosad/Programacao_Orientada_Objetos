dbc5d2c3d1327a3cb0b783b8e355477e93d4b1d3
Wesley Barbosa da Silva <124535880+wBarbosad@users.noreply.github.com>
2023-06-03 00:51:05 -0300

    Lista 9

--- Listas/Lista9/Ex.02/src/Banana.java
+++
@@ -1,2 +0,0 @@
-public class Banana implements Alimentos{
-}
--- Listas/Lista9/Ex.02/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Liquidificador<Banana> liquidificador = new Liquidificador();
-        liquidificador.bater();
-    }
-}
